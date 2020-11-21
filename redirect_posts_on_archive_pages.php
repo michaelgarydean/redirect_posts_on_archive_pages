d748ae367f2aca500d4358ed619930c957cd114b
Mike Dean <contact@michaeldean.ca>
2020-11-21 23:25:26 +0000

    initial commit

---
+++ redirect_posts_on_archive_pages.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Redirect Posts on Archive Pages
+ * Plugin URI:      https://github.com/mykedean/redirect_posts_on_archive_pages
+ * Description:     Redirect posts on an archive page with a custom URL.
+ * Author:          Michael Gary Dean <contact@michaeldean.ca>
+ * Author URI:      https://github.com/mykedean
+ * Text Domain:     redirect_posts_on_archive_pages
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Redirect_posts_on_archive_pages
+ */
